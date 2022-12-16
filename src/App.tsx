957e45dca54f77252b1fa9173bc1d373b03d16ee
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2022-12-16 09:58:53 -0600

    Added projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.scss';
+import Dashboard from './components/Dashboard/Dashboard';
 import NavBar from './components/NavBar/NavBar';
+import Projects from './components/Projects/Projects';
 import Reports from './components/Reports/Reports';
 
 const App = ({}) => {
   return (
     <main>
       <NavBar />
-      <Reports />
+      {/* <Dashboard /> */}
+      {/* <Reports /> */}
+      <Projects />
     </main>
   );
 };
---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,15 @@
+import SectionHeading from '../UI/SectionHeading';
+import './Projects.scss';
+
+interface Props {}
+
+const Projects = ({}) => {
+  return (
+    <section className="projects-section">
+      <SectionHeading title="Projects" />
+      <span>hello</span>
+    </section>
+  );
+};
+
+export default Projects;
