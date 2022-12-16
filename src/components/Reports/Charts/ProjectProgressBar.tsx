38e803dcc95a9ebf38eae4e8c39b12cf22bfcdf9
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2022-12-16 09:59:10 -0600

    Added project completion component for reports

---
+++ src/components/Reports/Charts/ProjectProgressBar.tsx
@@ -0,0 +1,14 @@
+import './ProjectProgressBar.scss';
+
+interface Props {}
+
+const ProjectProgressBar = ({}) => {
+  return (
+    <div className="container">
+      <span className="project-title">Project Title 1</span>
+      <div className="progress-bar"></div>
+    </div>
+  );
+};
+
+export default ProjectProgressBar;
